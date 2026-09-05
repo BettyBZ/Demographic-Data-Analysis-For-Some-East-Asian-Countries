a5c1b7bf954d460cacf7c1e9bdcb6eb0b7974fa3
BettyBZ <bettybotianzhang@yahoo.com>
2026-09-05 17:32:50 -0400

    Revise comments and set women's age range to 21-51
    Updated the comments and authorship information in the script. Adjusted the age range for women's population analysis.

--- Japan Demographic Structure Analysis/Ratio of 1 Years Old and Women's Population in an Age Range/Program(Ratio of 1 Years Old and Women's Population in an Age Range)/Plot(1_years_old_and_Specific_Women_Age_Range_Population_Ratio) .py
+++ Japan Demographic Structure Analysis/Ratio of 1 Years Old and Women's Population in an Age Range/Program(Ratio of 1 Years Old and Women's Population in an Age Range)/Plot(1_years_old_and_Specific_Women_Age_Range_Population_Ratio) .py
@@ -1,6 +1,23 @@
-#Plot(1_years_old_and_Specific_Women_Age_Range_Population_Ratio)
-# This code is partcially write by my own. 
-# I also past some part of the code from "Plot(%of_people_with_selected_sex_slected_year_in_city).py". This code is write by AI(Gemini).
+# Python 3
+# filename: Plot(1_years_old_and_Specific_Women_Age_Range_Population_Ratio)
+# Authors: Betty Botian Zhang, bz2443@nyu.edu
+# AI Assistant: Gemini
+"""This program plot the the ratio of 1 years old and women's Population 
+in an Age Range(e.g. 21-51 in this program). In the program, the year are
+substracted by 1, because the plot is attempted to show the the ratio of
+newborns and the ratio of women's population in an Age Range such as
+20 - 50 for the previous year of the measurement.
+Why I did not use the data for newborns that are 0 years old? It is because 
+there are huge error in the data for China, I am not sure if it is the case
+for all other population data. Also, the people before age 60 did not have a
+very low suvival rate after 10 years. I decide that the death rate in one 
+year is neglectable. I am currently using this method to measure how many
+percent of women in reproductive age (such as 20 - 50) give birth to a child
+in one year. I find that this is a strong indicator for the population project.
+It tells the reason for population ageing, and could help to predict future
+population trends. 
+"""
+## This code is partcially write by my own. I also past some part of the code from "Plot(%of_people_with_selected_sex_slected_year_in_city).py". This code is write by AI(Gemini).
 
 # Import
 import pandas as pd
@@ -9,8 +26,8 @@ import matplotlib.pyplot as plt
 # Set up condition
 selected_people = "Japanese"
 selected_nation = "Japan"
-women_selected_age_starts = 0
-women_selected_age_ends = 100
+women_selected_age_starts = 21
+women_selected_age_ends = 51
 
 # read file
 input_filename = f"{selected_nation} 1 year old and ({women_selected_age_starts}-{women_selected_age_ends}) Women Ratio(Each Year population sensus).csv"
